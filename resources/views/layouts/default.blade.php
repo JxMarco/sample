0e6a12bf47d90b2451cc6d21c5429a15a61b58f7
vagrant <vagrant@homestead>
2018-04-15 03:35:55 +0000

    跟着教程--完成页面布局和路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,17 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Sample App') - Laravel 新手入门教程</title>
-</head>
-<body>
-    @yield('content')
-</body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
